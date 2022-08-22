24c3f534be5bde1a03e177413962d13d495ee2a5
codingvaleria <codingvaleria@gmail.com>
2022-08-22 13:23:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Code your solution here
+
+function findMatching(driverNames, string) {
+    const result = driverNames.filter(function (driverName) {
+        if (driverName.toLowerCase() === string.toLowerCase()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    })
+
+    return result;
+}
+
+// function findMatching(driverNames, string) {
+//     return driverNames.filter(driverName => {
+//         if (driverName.toLowerCase() === string.toLowerCase()) {
+//             return true;
+//         }
+
+//         return false;
+
+//     })
+
+// }
+
+function fuzzyMatch(driverNames, string) {
+    const result = driverNames.filter(function (driverName) {
+        if (driverName.startsWith(string)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    )
+    return result;
+}
+
+function matchName(driverArray, string) {
+
+    const result = driverArray.filter(function (driverName) {
+        if (driverName.name === string) {
+            return true;
+        }
+    }
+    )
+    return result;
+}
+
+
+
+
+
+
